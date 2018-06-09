f072aadb6a339c9e8e2c0ccc798cc2c76e966e6f
bhaktik26 <bhakti.kapadia95@gmail.com>
2018-06-09 20:37:44 +0530

    ADDING COMMENT

--- src/codechef/misc/SumOfDigits.java
+++ src/codechef/misc/SumOfDigits.java
@@ -5,7 +5,8 @@ public class SumOfDigits {
 	public static void main(String[] args) {
 		int n = 234;
 		// String.valueOf will return the string representation of n. chars()
-		// converts it into IntStream
+		// converts it into IntStream, getNumericValue is the int value tht the
+		// character represents
 		int sum = String.valueOf(n).chars().map(Character::getNumericValue).sum();
 		System.out.println(sum);
 
